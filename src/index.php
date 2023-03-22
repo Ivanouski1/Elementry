ee196bcdd5063d03d12d3157e5cf537849e5fb57
Andrei Ivanouski1 <Andrei_Ivanouski1@epam.com>
2023-03-22 14:07:00 +0300

    test

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 <title>PHP Test</title>
 </head>
 <body>
-<?php echo '<p>DevOps assessment with Andrei Ivanouski</p>'; ?>
+<?php echo '<p>DevOps assessment with Andrei Ivanouski for Elementary</p>'; ?>
 </body>
 </html>
 <?php phpinfo(); ?>
